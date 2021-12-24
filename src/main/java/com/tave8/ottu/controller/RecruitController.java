21e0e200440b668bb695b22cb7fdf7e3c4b26137
youngniw <1999julie@naver.com>
2021-12-24 23:52:20 +0900

    modify gitignore

--- src/main/java/com/tave8/ottu/controller/RecruitController.java
+++ src/main/java/com/tave8/ottu/controller/RecruitController.java
@@ -1,19 +1,21 @@
 package com.tave8.ottu.controller;
 
-import com.tave8.ottu.entity.Recruit;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 @RestController
-@RequestMapping(value = "/recruit")
+@RequestMapping(value = "/recruit/{platform_id}")
 public class RecruitController {
     //모집 관련
     @GetMapping("/")
-    public ArrayList<Recruit> getRecruitApi() {
+    public ResponseEntity getRecruitApi() {
         //TODO: 수정 요망!
-        return null;
+        HashMap<String, Object> response = new HashMap<>();     //클라이언트에게 보낼 응답
+        return new ResponseEntity(response, HttpStatus.OK);
     }
 }
