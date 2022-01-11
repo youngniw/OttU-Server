d9b779aeb5246685481b5bb9470d9b446ee1bb11
youngniw <1999julie@naver.com>
2022-01-11 13:00:26 +0900

    server time zone change

--- src/main/java/com/tave8/ottu/OttuApplication.java
+++ src/main/java/com/tave8/ottu/OttuApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class OttuApplication {
@@ -12,4 +15,9 @@ public class OttuApplication {
 		SpringApplication.run(OttuApplication.class, args);
 	}
 
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+
+	}
 }
