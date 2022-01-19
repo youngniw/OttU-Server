2bb11414a013885af4bb661203efc21527fa6b29
youngniw <1999julie@naver.com>
2022-01-19 14:56:53 +0900

    Refactoring - server url, remove current postlist error

--- src/main/java/com/tave8/ottu/repository/PostRepository.java
+++ src/main/java/com/tave8/ottu/repository/PostRepository.java
@@ -13,19 +13,20 @@ public interface PostRepository extends JpaRepository <Post,Long> {
     @Query("SELECT p FROM Post p WHERE p.platform.platformIdx = :platformIdx AND p.isDeleted = false ORDER BY p.createdDate DESC")
     List<Post> findAllByPlatformIdx(@Param("platformIdx") int platformIdx);
 
+    //각 플랫폼 마다의 최신글을 받아옴-> 동일 시간이 있다면 post_idx가 높은 것이 조회됨
     @Query(value = "select * " +
             "from (" +
             "select * " +
             "from communitypost " +
             "where (platform_idx, post_idx) in ( " +
-            "select platform_idx, post_idx " +
+            "select platform_idx, max(post_idx) as post_idx " +
             "from communitypost " +
             "where (platform_idx, created_date) in (select platform_idx, max(created_date) as created_date from communitypost where is_deleted=false group by platform_idx) " +
             "group by platform_idx" +
             ")" +
             ") currentpost " +
             "order by platform_idx asc;", nativeQuery = true)
-    List<Post> findByCreatedDate();     //각 플랫폼 마다의 최신글을 받아옴
+    List<Post> findByCreatedDate();
 
     @Query("SELECT p FROM Post p WHERE p.writer.userIdx = :userIdx ORDER BY p.createdDate DESC")
     List<Post> findAllByUserIdx(@Param("userIdx") Long userIdx);
